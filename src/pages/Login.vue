4d83a15503d3629b8ba5a4d50609416df58e3eb3
Sueyoung Oh <sueyoung.oh@fitogether.com>
2020-09-08 14:33:44 +0900

    [Login] add login api request

--- src/pages/Login.vue
+++ src/pages/Login.vue
@@ -2,32 +2,66 @@
   <div class="bg">
     <div id="login" class="login">
       <img src="../assets/logo.png">
-      <div name="div_email">
+      <div name="div_email" class="login__row">
         <span>Email</span>
-        <input v-model="email" placeholder="abc@helloworld.com"/>
+        <input v-model="email" v-on:keyup="validateEmail" placeholder="abc@helloworld.com"/>
+        <div>
+          <span class="msg msg--warning">{{msg_email_invalid}}</span>
+        </div>
       </div>
-      <div name="div_password">
+      <div name="div_password" class="login__row">
         <span>Password</span>
         <input type="password" v-model="password" placeholder="abCD12!@"/>
       </div>
-      <button name="btn_login" v-on:click="login">LOGIN</button>
+      <div>
+        <button name="btn_login" v-on:click="login">LOGIN</button>
+      </div>
     </div>
   </div>
 </template>
 
 <script>
+const axios = require('axios')
+
+const DEV_API_URL = 'https://admin-api-dev.ohcoach.com'
+const EMAIL_REGEX = /^[^\s@]+@[^\s@]+\.[^\s@]+$/
+
 export default {
   name: 'Login',
-  el: '#login',
   data () {
     return {
       email: '',
-      password: ''
+      password: '',
+      auth: '',
+      loading: true,
+      msg_email_invalid: ''
     }
   },
   methods: {
+    validateEmail () {
+      let email = this.email
+      let matches = EMAIL_REGEX.exec(email)
+      if (!matches) {
+        this.msg_email_invalid = 'Wrong email address'
+      } else {
+        this.msg_email_invalid = ''
+      }
+    },
     login: function (event) {
-      alert('Login!!!!' + this.email + ' ' + this.password)
+      axios
+        .post(DEV_API_URL + '/auth/signin',
+          {
+            'email': this.email,
+            'password': this.password
+          })
+        .then(response => {
+          console.log(response)
+          this.auth = response.data.Authorization
+          alert('Welcome!')
+        })
+        .catch(error => {
+          alert('[Login Failed] ' + error)
+        })
     }
   }
 }
@@ -42,5 +76,14 @@ export default {
     width: 500px;
     height: 500px;
     display: inline-block;
+  }
+  .login__row {
+    height: 50px;
+  }
+  .msg {
+    font-size: 14px;
+  }
+  .msg--warning {
+    color: red;
   }
 </style>
